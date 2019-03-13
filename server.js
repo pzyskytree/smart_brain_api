2ddb74f3b86187bdb1af8d941b756de7a10c768f
pzyskytree <pzyskytree@gmail.com>
2019-03-12 20:22:44 -0400

    server

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/profile/:id", profile.handleProfile(db));
 app.put("/image", image.handleImage(db));
 app.post("/imageUrl", image.handleApiCall());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`app is running on port ${PORT}`);
 });
